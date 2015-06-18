6ec8fe04df6695717bb066d0146b975d27e75274
ACE-Caedmon <15914161012@163.com>
2015-06-18 17:14:20 +0800

    接口变更

--- src/main/java/com/ace/ng/dispatch/javassit/CmdHandlerCreator.java
+++ src/main/java/com/ace/ng/dispatch/javassit/CmdHandlerCreator.java
@@ -3,5 +3,5 @@ package com.ace.ng.dispatch.javassit;
 import com.ace.ng.dispatch.message.CmdHandler;
 
 public interface CmdHandlerCreator {
-	public CmdHandler<?> create(Short cmd);
+	CmdHandler<?> create(Short cmd);
 }
